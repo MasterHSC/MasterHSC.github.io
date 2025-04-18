84a60dc963e49fdb63af00e64d0a38590a5f4f4e
hsc <18812105192@163.com>
2025-04-18 16:07:32 +0800

    firstcommit

---
+++ common/tools/resize_photo.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+def resize_and_crop(image_path, output_path, target_width, target_height):
+    img = Image.open(image_path)
+    
+    original_width, original_height = img.size
+    
+    new_width = target_width
+    new_height = int(target_width * original_height / original_width)
+    img_resized = img.resize((new_width, new_height))
+
+    left = 0
+    top = (new_height - target_height) // 2
+    right = new_width
+    bottom = top + target_height
+
+    img_cropped = img_resized.crop((left, top, right, bottom))
+
+    img_cropped.save(output_path)
+
+image_path = 'common/img/group8.jpg' 
+output_path = image_path.replace('.jpg', '-resized.jpg')
+# output_path = image_path.replace('.png', '-resized.png')
+# pattern for people images
+# target_width = 300  
+# target_height = 400
+# patern for group images
+target_width = 400  
+target_height = 300 
+
+resize_and_crop(image_path, output_path, target_width, target_height)
